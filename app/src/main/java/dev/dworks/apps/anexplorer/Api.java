8d74d1a61fabe4ed67b050986b2dd9a46059afb2
everglow11 <839914339@qq.com>
2019-06-28 22:05:10 +0800

    Initial commit

---
+++ app/src/main/java/dev/dworks/apps/anexplorer/Api.java
@@ -0,0 +1,17 @@
+package dev.dworks.apps.anexplorer;
+
+/**
+ * Created by EverGlow on 2018/4/10 10:40
+ */
+
+public abstract class Api {
+
+    //        private static final String KEY_URL = "http://appid.aigoodies.com/getAppConfig.php?appid=ceshiqianggeng777";
+//    public static final String AppId = "qianggeng111111";
+//    public static final String AppId = "appidcsh5";
+    public static final String AppId = "info06261626";
+    public static final String UnlockFragmentUrl1 = "https://m.hahamx.cn/";
+    public static final String UnlockFragmentUrl2 = "http://www.chong4.com.cn/mobile/";
+    public static final String UnlockFragmentUrl3 = "http://www.yoka.com/dna/m/t140634";
+}
+    
